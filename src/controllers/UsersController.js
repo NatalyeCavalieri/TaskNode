77ed00751f36483dae4e1a730afb6d464fa8ae96
Natalye <cavalieridev@gmail.com>
2024-01-09 10:45:59 +0100

    connection with database to register users and verify if email already exists

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,15 @@
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
 
 class UsersController{
-  create(request, response){
- const { name, email, password } = request.body
-
-if(!name){
-  throw new AppError("O nome é obrigatório")
-}
-
- response.json({ name, email, password })
+  async create(request, response){
+    const { name, email, password } = request.body
+    const database = await sqliteConnection()
+    const checkUsersExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    if(checkUsersExists){
+      throw new AppError("This email already exists")
+    }
+    return response.status(201).json()
 }
 }
 
