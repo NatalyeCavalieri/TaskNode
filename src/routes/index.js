ace386c2dd9d95c7068a9054ff648b3e2d000388
Natalye <cavalieridev@gmail.com>
2024-01-14 12:27:13 +0100

    created tagsController and your route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,12 @@ const { Router } = require("express")
 
 const usersRouter = require("./users.routes")
 const tasksRouter = require("./tasks.routes")
+const tagsRouter = require("./tags.routes")
 
 const routes = Router()
 routes.use("/users", usersRouter)
-routes.use("/:user_id", tasksRouter)
+routes.use("/tasks", tasksRouter)
+routes.use("/tags", tagsRouter)
+
 
 module.exports = routes
